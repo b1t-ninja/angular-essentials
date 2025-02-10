529f5eb1d55360f89836297f624d28ddac15457b
b1t-ninja <nrueckner@outlook.de>
2025-02-10 12:19:15 +0100

    defining component inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import {Component, computed, signal} from '@angular/core'
-import {DUMMY_USERS} from "../dummy"
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import {Component, Input} from '@angular/core'
 
 @Component({
   selector: 'app-user',
@@ -11,12 +8,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex])
+  @Input({required: true}) avatar!: String
+  @Input({required: true}) name!: String
 
-  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`)
+  get imagePath() {
+    return `assets/users/${this.avatar}`
+  }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
   }
 }
