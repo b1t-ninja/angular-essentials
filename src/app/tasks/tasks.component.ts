27226b0f4abf99a0a5bb4c9f71b7285dca4594ae
b1t-ninja <nrueckner@outlook.de>
2025-02-16 12:53:07 +0100

    More service usage & alternative dependency injection

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {TaskComponent} from "./task/task.component";
 import {NewTaskComponent} from "./new-task/new-task.component";
-import {NewTaskData} from "./task/task.model";
 import {TasksService} from "./tasks.service";
 
 @Component({
@@ -31,11 +30,7 @@ export class TasksComponent {
     this.isAddingTask = true
   }
 
-  onCancelAddTask() {
+  onCloseAddTask() {
     this.isAddingTask = false
-  }
-
-  onAddTask(taskData: NewTaskData) {
-    this.tasksService.addTask(taskData, this.id!)
   }
 }
